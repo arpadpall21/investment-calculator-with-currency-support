1ac7e9941ea0547e3fed8e71f8eb2ff475bafc8f
arpadpall21 <a.pall21@yahoo.fr>
2025-06-15 08:15:37 +0200

    read toml file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 from tomllib import load
 
 
+def read_input_toml(path: str):
+    with open(path, 'br') as f:
+        return load(f)
+
+
 def calculate_investment():
-    return 0
+    input = read_input_toml('./input.toml')
+    print(input)
 
 
 if __name__ == "__main__":
