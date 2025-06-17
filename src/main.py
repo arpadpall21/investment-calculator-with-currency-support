970005ba1207cf970b92faae9e302713d98d4f33
arpadpall21 <a.pall21@yahoo.fr>
2025-06-17 09:33:48 +0200

    include currency at the end of lines

--- src/main.py
+++ src/main.py
@@ -22,53 +22,25 @@ def calculate_investment():
             gained_as_interest = sum[currency] / 100 * yearly_interest_rate
             total_at_year_end = sum[currency] + gained_as_interest
 
-            print(f"    Gained as interest: {round(gained_as_interest, displayed_decimal_length)}")
+            print(f"    Gained as interest: {round(gained_as_interest, displayed_decimal_length)} {currency}")
 
             if input["yearly_investment"].get(currency):
-                print(f"    Yearly investment: {input["yearly_investment"][currency]}")
+                print(f"    Yearly investment: {input["yearly_investment"][currency]} {currency}")
 
                 total_at_year_end_yearly_invest_inc = total_at_year_end + input["yearly_investment"][currency]
                 print(
-                    "    Total sum at the end of the year including yearly investment: " +
-                    f"{round(total_at_year_end_yearly_invest_inc, displayed_decimal_length)}"
+                    "    Total including yearly investment: " +
+                    f"{round(total_at_year_end_yearly_invest_inc, displayed_decimal_length)} {currency}"
                 )
                 sum[currency] = total_at_year_end_yearly_invest_inc
             else:
-                print(f"    Total sum at the end of the year: {round(total_at_year_end, displayed_decimal_length)}")
+                print(f"    Total: {round(total_at_year_end, displayed_decimal_length)} {currency}")
                 sum[currency] = total_at_year_end
 
-
-        # print(f"Yearly report in target currency ({input["output_curency"]})")
+        print(f"Total in target currency {input["output_curency"]}")
+        print(f"    Gained as interest: {input["output_curency"]}")
+        print(f"    Total: {input["output_curency"]}")
 
 
 if __name__ == "__main__":
     calculate_investment()
-
-
-# monthly_investment_huf = 700_000
-# initial_huf = 24_453_000 + 719618
-# initial_ron = 152_634 + 318_000
-
-# huf_exchange_rate = 0.0025
-# ron_exchange_rate = 0.20
-
-# current_total_eur = (initial_huf * huf_exchange_rate) + (initial_ron * ron_exchange_rate)
-# yearly_investment_eur = monthly_investment_huf * huf_exchange_rate * 12
-
-# print(f'Current total EUR: {current_total_eur}')
-# print(f'Yearly investment EUR: {yearly_investment_eur}')
-
-
-# def calc_investment(initial_sum, interest_rate, years, yearly_investment=0):
-#     result = initial_sum
-
-#     for year in range(years):
-#         current_interest = result / 100 * interest_rate
-#         print(f'Interest in year {year + 1}: {current_interest}')
-
-#         result = result + current_interest + yearly_investment
-
-#     return result
-
-
-# print(f'Result: {calc_investment(current_total_eur, 5, 20, yearly_investment_eur)}')
