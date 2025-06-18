fdc519984c4d993868c6099933ce6f6841347fec
arpadpall21 <a.pall21@yahoo.fr>
2025-06-18 23:35:16 +0200

    yearly interest caculation

--- src/main.py
+++ src/main.py
@@ -18,13 +18,15 @@ def calculate_investment():
 
     for investment_year in range(1, input["years_to_invest"]["years"] + 1):
         print(f"\nReport at the end of year {investment_year}")
+        yearly_interest_accumulation = {}
 
         for currency, yearly_interest_rate in input["yearly_interest_rate"].items():
             print(f"Currency: {currency} (yearly interest rate: {yearly_interest_rate}%)")
+
             gained_as_interest: float = total_accumulation[currency] / 100 * yearly_interest_rate
+            yearly_interest_accumulation[currency] = gained_as_interest
             print(f"    Gained as interest: {round(gained_as_interest, displayed_decimal_length)} {currency}")
 
-            # total_accumulation[currency]["gained_as_interest"] += gained_as_interest
             total_accumulation[currency] += gained_as_interest
             print(f"    Total: {round(total_accumulation[currency], displayed_decimal_length)} {currency}")
 
@@ -36,10 +38,14 @@ def calculate_investment():
                     f"{round(total_accumulation[currency], displayed_decimal_length)} {currency}"
                 )
 
-        total = cacl_accumulation_in_output_currency(total_accumulation, input["exchange_rate"], input["output_currency"])
+        total = cacl_accumulation_in_output_currency(total_accumulation,
+                                                     input["exchange_rate"], input["output_currency"])
+        total_yearly_interest_in_output_currency = cacl_accumulation_in_output_currency(yearly_interest_accumulation,
+                                                                                        input["exchange_rate"],
+                                                                                        input["output_currency"])
         print(f"Total in output currency {input["output_currency"]}")
-        # print(f"    Gained as interest: {round(interest, displayed_decimal_length)}" +
-        #       f"{input["output_currency"]}")
+        print("    Gained as interest this year: "
+              f"{round(total_yearly_interest_in_output_currency, displayed_decimal_length)} {input["output_currency"]}")
         print(f"    Total: {round(total, displayed_decimal_length)} {input["output_currency"]}")
 
 
