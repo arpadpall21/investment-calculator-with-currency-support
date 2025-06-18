f3d05200977207b5f4ab170ae25074699e6515de
arpadpall21 <a.pall21@yahoo.fr>
2025-06-18 23:22:18 +0200

    total & yearly interest refactor

--- src/main.py
+++ src/main.py
@@ -14,37 +14,33 @@ def calculate_investment():
 
     for currency, value in input["start_sum"].items():
         print(f"Start Sum {currency}: {value}")
-        total_accumulation[currency] = {"gained_as_interest": 0, "total": input["start_sum"][currency]}
+        total_accumulation[currency] = value
 
     for investment_year in range(1, input["years_to_invest"]["years"] + 1):
         print(f"\nReport at the end of year {investment_year}")
 
         for currency, yearly_interest_rate in input["yearly_interest_rate"].items():
             print(f"Currency: {currency} (yearly interest rate: {yearly_interest_rate}%)")
-            gained_as_interest: float = total_accumulation[currency]["total"] / 100 * yearly_interest_rate
+            gained_as_interest: float = total_accumulation[currency] / 100 * yearly_interest_rate
             print(f"    Gained as interest: {round(gained_as_interest, displayed_decimal_length)} {currency}")
 
-            total_accumulation[currency]["gained_as_interest"] += gained_as_interest
-            total_accumulation[currency]["total"] += gained_as_interest
-            print(f"    Total: {round(total_accumulation[currency]["total"], displayed_decimal_length)} {currency}")
+            # total_accumulation[currency]["gained_as_interest"] += gained_as_interest
+            total_accumulation[currency] += gained_as_interest
+            print(f"    Total: {round(total_accumulation[currency], displayed_decimal_length)} {currency}")
 
             if input["yearly_investment"].get(currency):
                 print(f"    Yearly investment: {input["yearly_investment"][currency]} {currency}")
-                total_accumulation[currency]["total"] += input["yearly_investment"][currency]
+                total_accumulation[currency] += input["yearly_investment"][currency]
                 print(
                     "    Total including yearly investment: " +
-                    f"{round(total_accumulation[currency]["total"], displayed_decimal_length)} {currency}"
+                    f"{round(total_accumulation[currency], displayed_decimal_length)} {currency}"
                 )
 
-        gained_as_interest_yearly, total_yearly = cacl_accumulation_in_output_currency(total_accumulation,
-                                                                                       input["exchange_rate"],
-                                                                                       input["output_currency"])
+        total = cacl_accumulation_in_output_currency(total_accumulation, input["exchange_rate"], input["output_currency"])
         print(f"Total in output currency {input["output_currency"]}")
-        print(f"    Gained as interest: {round(gained_as_interest_yearly, displayed_decimal_length)}" +
-              f"{input["output_currency"]}")
-        print(f"    Total: {round(total_yearly, displayed_decimal_length)} {input["output_currency"]}")
-
-    print(total_accumulation)
+        # print(f"    Gained as interest: {round(interest, displayed_decimal_length)}" +
+        #       f"{input["output_currency"]}")
+        print(f"    Total: {round(total, displayed_decimal_length)} {input["output_currency"]}")
 
 
 if __name__ == "__main__":
--- src/misc/calc_accumulation.py
+++ src/misc/calc_accumulation.py
@@ -2,21 +2,15 @@ def cacl_accumulation_in_output_currency(
     accumulation: dict,
     exchange_rate: dict,
     output_currency: str
-) -> tuple[float, float]:
-    total_interest = 0
-    total = 0
+) -> float:
+    result = 0
 
-    for currency, report in accumulation.items():
+    for currency, val in accumulation.items():
         if currency == output_currency:
-            total_interest += report["gained_as_interest"]
-            total += report["total"]
+            result += val
             continue
-
         if not exchange_rate.get(currency):
             raise ValueError(f"Exchange rate missing for currency {currency}")
+        result += val / exchange_rate[currency]
 
-        total_interest += report["gained_as_interest"] / exchange_rate[currency]
-        total += report["total"] / exchange_rate[currency]
-
-
-    return total_interest, total
+    return result
