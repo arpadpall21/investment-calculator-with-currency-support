5d02ad49e77552d62fcd0c56d6ce62ea601e118a
arpadpall21 <a.pall21@yahoo.fr>
2025-06-20 06:55:24 +0200

    display yearly investment upgraded

--- src/main.py
+++ src/main.py
@@ -6,10 +6,15 @@ from misc.print import prt, fmt_float
 def calculate_investment():
     total_accumulation: dict = {}
 
-    prt("Start Sum:", color="yellow")
+    prt("Start sum:", color="yellow")
     for currency, value in input["start_sum"].items():
         prt(f"{fmt_float(value)} {currency}", tabs=1)
         total_accumulation[currency] = value
+
+    if len(input["yearly_investment"]) > 0:
+        prt("Yearly investment:", color="yellow")
+        for currency, value in input["yearly_investment"].items():
+            prt(f"{fmt_float(value)} {currency}", tabs=1)
 
     for investment_year in range(1, input["years_to_invest"]["years"] + 1):
         prt(f"Report at the end of year {investment_year}", new_line=True, color="magenta")
