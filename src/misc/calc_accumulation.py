9b8806ae9065e6f40e6648a389b9dd7c7ec1469f
Arpad <arpad.pall@zeiss.com>
2025-12-28 09:31:22 +0200

    handle exchange rate case

--- src/misc/calc_accumulation.py
+++ src/misc/calc_accumulation.py
@@ -2,7 +2,13 @@ from misc.input import input_toml
 
 
 def to_output_currency(val: float, currency: str) -> float:
-    if not input_toml["exchange_rate"].get(currency):
-        raise ValueError(f"Exchange rate missing for currency {currency}")
+    exchange_rate: float = 0.0
+    if currency == input_toml["output_currency"]:
+        exchange_rate = 1.0
+    else:
+        _exchange_rate: float | None = input_toml["exchange_rate"].get(currency)
+        if _exchange_rate is None:
+            raise ValueError(f"Exchange rate missing for currency {currency}")
+        exchange_rate = _exchange_rate
 
-    return val / input_toml["exchange_rate"][currency]
+    return val / exchange_rate
